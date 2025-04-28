e649a8a3e44deab1348a63ef042b624dae662399
Juan Maestre <98894997+JuandiGo1@users.noreply.github.com>
2025-04-28 09:57:59 -0500

    feat: add user photo and default avatar

--- src/explore/pages/ExplorePage.jsx
+++ src/explore/pages/ExplorePage.jsx
@@ -1,15 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../auth/hooks/useAuth";
+import defaultAvatar from "../../assets/defaultAvatar.jpg";
 
 const ExplorePage = () => {
   const { logout, user } = useAuth();
   const navigate = useNavigate();
 
+  const profileImage = user.photoURL || defaultAvatar;
+
   const handleLogout = async () => {
     try {
       await logout();
       console.log("Cierre de sesión exitoso.");
-      navigate("/"); 
+      navigate("/");
     } catch (error) {
       console.error("Error cerrando sesión:", error.message);
     }
@@ -18,12 +21,20 @@ const ExplorePage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#EAE0D5]">
       <h1>¡Hola, {user.displayName || "Usuario"}!</h1>
-      <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded">
+      <img
+        src={profileImage}
+        alt="Foto de perfil"
+        className="w-10 h-10 rounded-full object-cover"
+      />
+      <button
+        onClick={handleLogout}
+        className="bg-red-500 text-white px-4 py-2 rounded"
+      >
         Logout
       </button>
       <p>Welcome to the Explore Page!</p>
     </div>
   );
-}
+};
 
 export default ExplorePage;
