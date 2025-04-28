c2f83131fc94caf0799e7191b4a60a08a4a06b77
Juan Maestre <98894997+JuandiGo1@users.noreply.github.com>
2025-04-28 09:48:45 -0500

    style: change bg color explorepage

--- src/explore/pages/ExplorePage.jsx
+++ src/explore/pages/ExplorePage.jsx
@@ -16,7 +16,7 @@ const ExplorePage = () => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen bg-[#EAE0D5]">
       <h1>¡Hola, {user.displayName || "Usuario"}!</h1>
       <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded">
         Logout
