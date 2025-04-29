00089e44662620f4ad3abf0c392ef782dd6edf96
Juan Maestre <98894997+JuandiGo1@users.noreply.github.com>
2025-04-28 19:17:06 -0500

    feat: add Singin and singup btn in home public page

--- src/common/components/Navbar.jsx
+++ src/common/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { TiBookmark } from "react-icons/ti";
 
 const Navbar = ({ children }) => {
   const { logout, user } = useAuth();
-  const profileImage = user && user.photoURL ? user.photoURL : defaultAvatar;
+  const profileImage = user?.photoURL || defaultAvatar;
   const navigate = useNavigate();
 
   const handleLogout = async () => {
@@ -21,7 +21,7 @@ const Navbar = ({ children }) => {
   };
 
   return (
-    <div className="flex min-h-screen bg-[#EAE0D5]">
+    <div className="flex min-h-screen bg-[# ]">
       <nav className="flex flex-1 flex-col items-center justify-between bg-[#1c2930] text-white p-6 shadow-md">
         <div className="flex flex-col items-start justify-between w-full gap-10">
           <div className="flex items-center justify-start gap-2 mb-3">
@@ -31,17 +31,17 @@ const Navbar = ({ children }) => {
               className="w-10 h-10 rounded-full object-cover"
             />
             <h1 className="text-bold text-xl">
-              {user ? user.displayName : "Usuario"}
+              {user ? user.displayName : "¡Join Us today!"}
             </h1>
           </div>
-          <ul className="flex flex-col justify-start gap-7 text-lg">
+          <ul className="flex flex-col justify-start gap-7 text-lg w-full">
             <li>
               <NavLink
                 to="/home"
                 className={({ isActive }) =>
                   isActive
-                    ? "text-[#FFD700] flex items-center gap-2"
-                    : "flex items-center gap-2"
+                    ? "bg-[#EAE0D5]/40 pl-2 py-1 rounded-xl w-full flex items-center gap-2"
+                    : "flex items-center gap-2 pl-2"
                 }
               >
                 <BiHomeAlt2 className="text-xl" /> Home
@@ -52,8 +52,8 @@ const Navbar = ({ children }) => {
                 to="/profile"
                 className={({ isActive }) =>
                   isActive
-                    ? "text-[#FFD700] flex items-center gap-2"
-                    : "flex items-center gap-2"
+                    ? "bg-[#EAE0D5]/40 pl-2 py-1 rounded-xl w-full flex items-center gap-2"
+                    : "flex items-center gap-2 pl-2"
                 }
               >
                 <RiUser5Line className="text-xl" />
@@ -65,8 +65,8 @@ const Navbar = ({ children }) => {
                 to="/favorites"
                 className={({ isActive }) =>
                   isActive
-                    ? "text-[#FFD700] flex items-center gap-2"
-                    : "flex items-center gap-2"
+                    ? "bg-[#EAE0D5]/40 pl-2 py-1 rounded-xl w-full flex items-center gap-2"
+                    : "flex items-center gap-2 pl-2"
                 }
               >
                 <TiBookmark className="text-xl" />
@@ -76,27 +76,31 @@ const Navbar = ({ children }) => {
           </ul>
         </div>
         <div className="flex items-start w-full ">
-          {user? 
+          {user ? (
             <button
               onClick={handleLogout}
-              className="flex items-center justify-start gap-2 bg-red-500 text-white px-4 py-2 rounded"
+              className="flex items-center justify-start gap-2  text-white px-4 py-2 rounded-xl hover:bg-[#806248]/10 cursor-pointer"
             >
               <RiLogoutCircleLine className="text-xl" />
               Logout
             </button>
-          :
-            <NavLink
-              to="/login"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-[#FFD700] flex items-center gap-2"
-                  : "flex items-center gap-2"
-              }
-            >
-              <RiLogoutCircleLine className="text-xl" />
-              Login
-            </NavLink>
-          }
+          ) : (
+            <div className="flex flex-col justify-between gap-2">
+              <NavLink
+                to="/"
+                className="flex items-center gap-2 bg-[#a86428] text-white text-bold text-xl px-3 py-2 rounded-xl"
+              >
+                Sing In
+              </NavLink>
+
+              <NavLink
+                to="/"
+                className="flex items-center gap-2 bg-[#806248] text-white text-bold text-xl px-3 py-2 rounded-xl"
+              >
+                Sing Up
+              </NavLink>
+            </div>
+          )}
         </div>
       </nav>
       <main className="flex-6">{children}</main>
