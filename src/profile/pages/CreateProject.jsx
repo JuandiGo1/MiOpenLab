8446776ed76494aa868a79536b8f411e23600da5
Juan Maestre <98894997+JuandiGo1@users.noreply.github.com>
2025-04-30 19:53:08 -0500

    feat: add create project page

---
+++ src/profile/pages/CreateProject.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreatePostField from "../../common/components/CreatePostField";
+
+const CreateProject = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <CreatePostField />
+    </div>
+  );
+};
+
+export default CreateProject;
