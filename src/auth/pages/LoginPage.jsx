bfbc29c750bef8974cfd8c50164b53143e188327
JaymedDLC <jaymedl@uninorte.edu.co>
2025-05-07 19:09:24 -0500

    added buttom to login page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import LoginForm from "../components/LoginForm";
 import SignupForm from "../components/SignupForm";
+import DarkSwitch from "../../common/components/ThemeSwitch";
 
 const LoginPage = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <div className="flex flex-col sm:flex-row w-screen h-screen overflow-hidden ">
+    <div className="flex flex-col sm:flex-row w-screen h-screen overflow-hidden relative">
+      {/* DarkSwitch Button */}
+      <div className="absolute top-4 right-4">
+        <DarkSwitch />
+      </div>
+
       {/* Left Section */}
-      <div className="flex-1 bg-[#1c2930] dark:bg-black dark:text-red-600 text-white flex flex-col justify-center items-center p-4">
+      <div className="flex-1 bg-[#1c2930] flex flex-col justify-center items-center text-white p-4">
         <h1 className="text-4xl sm:text-5xl font-bold mb-2 sm:mb-4">
           MiOpenLab
         </h1>
