a03373b7784d2e25d126f25b005225d37a188278
Juan Maestre <98894997+JuandiGo1@users.noreply.github.com>
2025-05-22 15:27:51 -0500

    style: update ForgotPassword page stlyle

--- src/auth/pages/ForgotPassworPage.jsx
+++ src/auth/pages/ForgotPassworPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 
 const ForgotPasswordPage = () => {
-  const { resetPass} = useAuth();
+  const { resetPass } = useAuth();
   const [email, setEmail] = useState("");
   const [msgInfo, setMsgInfo] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -18,48 +18,48 @@ const ForgotPasswordPage = () => {
     } else {
       setMsgInfo(res.message);
     }
-    setIsLoading(false); 
+    setIsLoading(false);
   };
 
   return (
-    <div className="w-full max-w-full sm:max-w-md bg-[#EAE0D5] p-8 rounded-lg dark:bg-gray-800 shadow-lg">
-      <h1>Forgot Password</h1>
-      <p>Please enter your email to reset your password.</p>
-      <form onSubmit={handleResetPassword} className="space-y-4">
-        <div>
-          <label className="block text-sm font-medium text-gray-700 dark:text-gray-200">
-            Email
-          </label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="mt-1 block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none"
-            placeholder="Enter your email"
-            required
+    <div className="w-full max-w-full h-screen bg-[#1c2930] p-8 rounded-lg dark:bg-gray-800 justify-center items-center flex">
+      <div className="flex flex-col gap-3 w-1/3 items-center justify-center bg-[#EAE0D5] p-8 rounded-xl dark:bg-gray-800 shadow-lg ">
+        <h1 className="font-bold text-2xl ">Forgot Password</h1>
+        <p className="">Please enter your email to reset your password.</p>
+        <form onSubmit={handleResetPassword} className="space-y-4">
+          <div>
+
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="mt-1 block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none"
+              placeholder="Enter your email"
+              required
+              disabled={isLoading}
+            />
+          </div>
+          <button
+            type="submit"
             disabled={isLoading}
-          />
-        </div>
-        <button
-          type="submit"
-          disabled={isLoading}
-          className={`w-full bg-[#bd9260] text-white px-4 py-2 rounded-lg hover:bg-[#ca9c6e] transition duration-150 ease-in-out 
+            className={`w-full bg-[#bd9260] text-white px-4 py-2 rounded-lg hover:bg-[#ca9c6e] transition duration-150 ease-in-out 
             flex items-center justify-center min-w-[150px] min-h-[40px] ${
               isLoading ? "opacity-50 cursor-not-allowed" : ""
             }`}
-        >
-          {isLoading ? "Sending..." : "Send Reset Email"}
-        </button>
-        {msgInfo && (
-          <p
-            className={`text-center mt-4 ${
-              msgInfo.includes("Error") ? "text-red-500" : "text-green-500"
-            }`}
           >
-            {msgInfo}
-          </p>
-        )}
-      </form>
+            {isLoading ? "Sending..." : "Send Reset Email"}
+          </button>
+          {msgInfo && (
+            <p
+              className={`text-center mt-4 ${
+                msgInfo.includes("Error") ? "text-red-500" : "text-gray-800"
+              }`}
+            >
+              {msgInfo}
+            </p>
+          )}
+        </form>
+      </div>
     </div>
   );
 };
